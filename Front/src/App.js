ae62d988e89d779c59f68466e0dab4d3599166d6
JABerbary <augustoberbary@gmail.com>
2024-02-14 02:37:56 -0300

    ajuste do cors

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "./components/Sidebar//DrawerComponent";
-import CssBaseline from "@mui/material/CssBaseline";
+// import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "./components/Sidebar/ToolbarComponent";
 import "./App.css";
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Box sx={{ display: "flex" }}>
-        <CssBaseline />
+        {/* <CssBaseline /> */}
         <Toolbar />
         <Drawer />
       </Box>
